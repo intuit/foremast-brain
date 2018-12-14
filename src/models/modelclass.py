300be1a587d6105581824af41f1c513320c6b2c7
Ethan McCue <emccue@live.com>
2018-12-14 16:47:42 -0500

    Made getModelByKey and setModelKV usable via indexing

--- src/models/modelclass.py
+++ src/models/modelclass.py
@@ -14,26 +14,40 @@ class ModelHolder:
         self.period = period
         self.modelconfig = modelconfig
         self._id = id
+
     def getModelByKey(self, key):
         return self.modeldata.get(key)
+
     def setModelKV(self, key, value):
         self.modeldata.setdefault(key, value) 
+
     def hasModel(self):
         return len(self.modeldata)>0
+
     def getModelConfigByKey(self, key):
         return self.modelconfig.get(key)
+
     def setModelConfig(self, key ,value):
         self.modelconfig.setdefault(key,value)
+
     def setModelName(self, name):
         self.modelname = name
+
     def resetModel(self, data):
         self.modeldata = data
+
     def setModelMetadata(self, metadata):
         self.metadata = metadata
 
     @property
     def id(self):
         return self._id
+
+    def __getitem__(self, item):
+        return self.getModelByKey(item)
+
+    def __setitem__(self, key, value):
+        self.setModelKV(key, value)
 
     def __str__(self):
         sb = []
