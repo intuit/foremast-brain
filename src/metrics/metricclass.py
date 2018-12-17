9d712fe880ea0a67eb88c4f4f200a7fd8810d74f
Ethan McCue <emccue@live.com>
2018-12-16 20:57:50 -0500

    Subclasses should call the superclass constructor

--- src/metrics/metricclass.py
+++ src/metrics/metricclass.py
@@ -9,31 +9,31 @@ class MetricInfo:
 class SingleMetricInfo(MetricInfo):
     def __init__(self, metricName, metricKeys, columnmap, metricDF, metricTCategory=METRIC_PERIOD.CURRENT.value,
                  metricClass='SingleMetricInfo'):
+        super().__init__(metricClass)
         self.columnmap = columnmap
         self.metricDF = metricDF
         self.metricName = metricName
         self.metricKeys = metricKeys
         self.metricTCategory = metricTCategory
-        self.metricClass = metricClass
 
 
 class MultiTypeMetricInfo(MetricInfo):
     def __init__(self, metricNamelist, metricKeys, columnmap, metricDF, metricTCategorylist,
                  metricClass='MultiTypeMetricInfo'):
+        super().__init__(metricClass)
         self.columnmap = columnmap
         self.metricNamelist = metricNamelist
         self.metricKeys = metricKeys
         self.metricDF = metricDF
         self.metricTCategorylist = metricTCategorylist
-        self.metricClass = metricClass
 
 
 class MultiKeyMetricInfo(MetricInfo):
     def __init__(self, metricName, metricKeyslist, columnmap, metricDF, metricTCategorylist,
                  metricClass='MultiKeyMetricInfo'):
+        super().__init__(metricClass)
         self.columnmap = columnmap
         self.metricName = metricName
         self.metricKeyslist = metricKeyslist
         self.metricDF = metricDF
         self.metricTCategorylist = metricTCategorylist
-        self.metricClass = metricClass
