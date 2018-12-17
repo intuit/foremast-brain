8f9bb9846c3d63a7fde3f38859a854eab9d379a5
Ethan McCue <emccue@live.com>
2018-12-16 21:17:29 -0500

    Remove commented out "test"

--- src/metadata/metadata.py
+++ src/metadata/metadata.py
@@ -46,16 +46,3 @@ class AI_MODEL(Enum):
     PROPHET = 'prophet'
     BIVARIANT_GAUSSIAN = 'bivariant_gaussian'
     LSTM = 'lstm'
-
-##Test ##   
-# ll  = list(REQUEST_STATE)
-# print(len(ll))
-# print(ll)
-# dd =[c.value for c in REQUEST_STATE]
-# print(dd)
-# ww= REQUEST_STATE.COMPLETED_HEALTH
-# print(type(REQUEST_STATE.COMPLETED_HEALTH.value))
-# if (ww == REQUEST_STATE.COMPLETED_HEALTH):
-#    print("True")
-# else:
-#    print("False")
