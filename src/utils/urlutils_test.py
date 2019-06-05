80ab86ca6ce3c96e874368ac98be39335d9b1746
pzou <ping_zou@intuit.com>
2019-06-04 20:15:48 -0700

    add test cases

--- src/utils/urlutils_test.py
+++ src/utils/urlutils_test.py
@@ -2,8 +2,11 @@ import sys
 sys.path.append('../')
 from utils.urlutils import dorequest
 
+import unittest
 
-#str = dorequest("http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/query_range?query=namespace_app%3Ahttp_server_requests_error_4xx%7Bnamespace%3D%22default%22%2C+app%3D%22k8s-metrics-demo%22%7D&start=1540925404&end=1541530204&step=60")
-str = dorequest("http://www.google.com")
-print(str)
+class utlutilsMethods(unittest.TestCase):
+    def testurlutils(self):
+        #str = dorequest("http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/query_range?query=namespace_app%3Ahttp_server_requests_error_4xx%7Bnamespace%3D%22default%22%2C+app%3D%22k8s-metrics-demo%22%7D&start=1540925404&end=1541530204&step=60")
+        str = dorequest("http://www.google.com")
+        #print(str)
 
