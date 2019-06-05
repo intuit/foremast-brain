ff59256310f2ae4a973c893c2a74847f242066b0
pzou <ping_zou@intuit.com>
2019-06-04 20:27:46 -0700

    check in

--- src/metadata/globalconfig_test.py
+++ src/metadata/globalconfig_test.py
@@ -13,6 +13,11 @@ class globalconfig_test(unittest.TestCase):
     result = {'a': 1, 'b': 3, 'dd': 4}
     self.assertEqual(y.getKVs(), result)
     
+    data =x.getValueByKey('a', defaultValue = "aa")
+    data1 = x.getKVs()
+    data2 = x.getThresholdByKey("a","a")
+    data3 = x.setThresholdKV("a","a","value")
+    
     
     
 if __name__ == '__main__':
