3917dcc9e483d2b1fb46cc707c65fd28ca37e30c
pzou <ping_zou@intuit.com>
2019-06-05 18:38:03 -0700

    check in

--- src/utils/urlutils_test.py
+++ src/utils/urlutils_test.py
@@ -1,5 +1,4 @@
-import sys
-sys.path.append('../')
+
 from utils.urlutils import dorequest
 
 import unittest
@@ -7,6 +6,7 @@ import unittest
 class utlutilsMethods(unittest.TestCase):
     def testurlutils(self):
         #str = dorequest("http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/query_range?query=namespace_app%3Ahttp_server_requests_error_4xx%7Bnamespace%3D%22default%22%2C+app%3D%22k8s-metrics-demo%22%7D&start=1540925404&end=1541530204&step=60")
-        str = dorequest("http://www.google.com")
+         self.assertEqual(dorequest("http://www.google1.com"),'')
+         
         #print(str)
 
