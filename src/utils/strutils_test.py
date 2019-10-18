c3350a14be127da1e9b7111da504d26946ba222f
Manoj Yadav <man007yadav@gmail.com>
2019-10-18 23:23:34 +0530

    fix

--- src/utils/strutils_test.py
+++ src/utils/strutils_test.py
@@ -6,6 +6,5 @@ print(origstr)
 print(strReplace(origstr, '&end=', '&step=', '1234567890'))
 
 print(  findSubStr(origstr, 'query_range?query=', '&start='))
-
 ss="{'error4xx':[{'metric':{'__name__': 'namespace_pod:http_server_requests_error_4xx', 'namespace': 'default', 'pod': 'c-69c9dbb5d-hlv27'},'value':[[1542069372]]}],'latency':[{'metric':{'__name__': 'namespace_pod:http_server_requests_latency', 'namespace': 'default', 'pod': 'c-69c9dbb5d-hlv27'},'value':[[1542069372]]}]}"
 print(queryEscape(ss))
