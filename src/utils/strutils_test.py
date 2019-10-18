81921ccf0106fa9b76765e6b98ebf75aa88a5b27
Manoj Yadav <man007yadav@gmail.com>
2019-10-18 23:21:33 +0530

    fix

--- src/utils/strutils_test.py
+++ src/utils/strutils_test.py
@@ -9,5 +9,3 @@ print(  findSubStr(origstr, 'query_range?query=', '&start='))
 
 ss="{'error4xx':[{'metric':{'__name__': 'namespace_pod:http_server_requests_error_4xx', 'namespace': 'default', 'pod': 'c-69c9dbb5d-hlv27'},'value':[[1542069372]]}],'latency':[{'metric':{'__name__': 'namespace_pod:http_server_requests_latency', 'namespace': 'default', 'pod': 'c-69c9dbb5d-hlv27'},'value':[[1542069372]]}]}"
 print(queryEscape(ss))
-
-
